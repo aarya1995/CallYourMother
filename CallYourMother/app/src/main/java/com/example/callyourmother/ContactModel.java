4928b21afe69c0523fa4050b49b53ace266d2458
Abhas Arya <abhasarya@Abhass-MacBook-Pro.local>
2017-11-15 22:19:32 -0500

    configure database and create database and contacts API

---
+++ CallYourMother/app/src/main/java/com/example/callyourmother/ContactModel.java
@@ -0,0 +1,69 @@
+package com.example.callyourmother;
+
+/**
+ * Created by abhasarya on 11/15/17.
+ */
+
+public class ContactModel {
+    private String ID, firstName, lastName, phoneNumber, priority;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
